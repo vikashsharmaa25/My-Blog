e2f3f15c2813c2b1504eb096640f6aa314b007cb
Vikash Sharma <104788351+vikashsharmaa25@users.noreply.github.com>
2025-06-29 18:57:39 +0530

    Update middleware.ts

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -62,4 +62,6 @@ export function middleware(request: NextRequest) {
   return NextResponse.redirect(new URL("/", request.url));
 }
 
-middleware.matcher = ["/((?!_next/static|_next/image|favicon.ico).*)"];
+export const config = {
+  matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
+};
