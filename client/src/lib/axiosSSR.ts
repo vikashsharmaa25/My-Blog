03be1c8edf6812c6b7e0acf29dcec2c344ddcc1a
Vikash Sharma <104788351+vikashsharmaa25@users.noreply.github.com>
2025-06-29 19:01:39 +0530

    Update axiosSSR.ts

--- client/src/lib/axiosSSR.ts
+++ client/src/lib/axiosSSR.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosSSR = (cookie: string) =>
   axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://my-blog-1jl4.vercel.app/api",
     headers: {
       "Content-Type": "application/json",
       Cookie: cookie,
